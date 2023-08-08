4867b7acc1c763684f6ef8dcc9aa0f3151659ff7
Denilton Araújo <121826005+d2jtech@users.noreply.github.com>
2023-08-08 08:19:54 -0300

    ok

---
+++ CursoJS/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 8
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if(vel>60){
+    console.log("Você ultrapassou a velocidade permitida. Multado!")
+}
+console.log("Dirija sempre usando o cinto de segurança!")
+
