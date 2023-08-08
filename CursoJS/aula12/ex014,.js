be6248cbe8eb8d95df24882f2ebd36338e145670
Denilton Araújo <121826005+d2jtech@users.noreply.github.com>
2023-08-08 14:40:30 -0300

    exercicio

--- CursoJS/aula12/ex014,.js
+++ CursoJS/aula12/ex014,.js
@@ -1,4 +1,32 @@
 var agora = new Date()
 var diaSem = agora.getDay()
 
-console.log(diaSem)
+console.log(`Hoje é o dia ${diaSem}`)
+
+switch(diaSem){
+    case 0:
+        console.log('Hoje é Domingo')
+        break
+    case 1:
+        console.log('Hoje é Segunda')
+        break
+    case 2:
+        console.log('Hoje é Terça')
+        break
+    case 3:
+        console.log('Hoje é Quarta')
+        break
+    case 4:
+        console.log('Hoje é Quinta')
+        break
+    case 5:
+        console.log('Hoje é Sexta')
+        break
+    case 6:
+        console.log('Hoje é Sábado')
+        break
+    
+    default:
+        console.log('ERRO. DIA INVÁLIDO')
+        break
+}
