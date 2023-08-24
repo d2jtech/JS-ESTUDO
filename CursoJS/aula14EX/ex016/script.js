8f31129894b3856df5742960eb5993672b26a824
Denilton Araújo <121826005+d2jtech@users.noreply.github.com>
2023-08-24 09:55:03 -0300

    inicio

--- CursoJS/aula14EX/ex016/script.js
+++ CursoJS/aula14EX/ex016/script.js
@@ -1,1 +1,9 @@
-alert('Olá!!!!')
+function contar(){
+    var inicio = document.querySelector('div#txini')
+    var fim = document.querySelector('div#txfim')
+    var passo = document.querySelector('div#txpasso')
+    var res = document.querySelector('div#res')
+
+    alert(`Dados alocados  inicio:${inicio}  ,Fim:${fim}  , Passo:${passo} lincados e validados!`)
+
+}
