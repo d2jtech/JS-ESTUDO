a5a4b5470ff49d62bad02149c3f049b7fa52f5d4
Denilton Araújo <121826005+d2jtech@users.noreply.github.com>
2023-08-28 11:49:37 -0300

    o

--- CursoJS/aula14EX/ex016/script.js
+++ CursoJS/aula14EX/ex016/script.js
@@ -5,19 +5,27 @@ function contar(){
     let res = document.getElementById('res')
 
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert('[ERRO] Faltam dados!')
+        
+        res.innerHTML = 'IMPOSSÍVEL CONTAR!'
+        //window.alert('[ERRO] Faltam dados!')
+        
     } else {
-        res.innerHTML = 'Contando: ' 
+        res.innerHTML = 'Contando: <br>' 
         let i = Number(ini.value)
         let f = Number(fim.value)   
         let p = Number(passo.value)
 
+        if(p <=0){
+            window.alert('Passo Inválido! Considerando Passo 1')
+            p = 1
+        }
+
         if(i < f){
-            for(let c =i; c<=f; c+=p){
-                res.innerHTML += `${c} \u{1F449}`
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1F449}`
             }
-        } else{
-            for (let c = i; c<=f; c +=p){
+        } else {
+            for (let c = i; c >= f; c -= p){
                 res.innerHTML += ` ${c} \u{1F449}`
             }
         }
