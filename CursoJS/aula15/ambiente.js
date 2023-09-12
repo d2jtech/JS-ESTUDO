133ab1b87230673dd91f06e36abfd61a600f0081
Denilton Araújo <121826005+d2jtech@users.noreply.github.com>
2023-09-12 16:55:34 -0300

    op

---
+++ CursoJS/aula15/ambiente.js
@@ -0,0 +1,15 @@
+let num = [5,8,4,10,20,3,8]
+
+for(let pos = 0;pos<num.length;pos++){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+console.log("Uso do FOR IN")
+
+num.sort()
+
+for(let pos in num){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+
